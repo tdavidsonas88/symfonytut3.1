da4ef0c69556d7d7238375a97cd0a10450f7ec0c
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2018-02-12 21:05:46 +0200

    symfony 3 crud app

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PostController extends Controller
+{
+    /**
+     * @Route("/post", name="view_post_route")
+     */
+    public function viewPostAction() {
+        return $this->render("pages/index.html.twig");
+    }
+    /**
+     * @Route("/post/create", name="create_post_route")
+     */
+    public function createPostAction() {
+        return $this->render("pages/create.html.twig");
+    }
+    /**
+     * @Route("/post/update/{id}", name="update_post_route")
+     */
+    public function updatePostAction($id, Request $request) {
+        return $this->render("pages/update.html.twig");
+    }
+    /**
+     * @Route("/post/show/{id}", name="show_post_route")
+     */
+    public function showPostAction($id, Request $request) {
+        return $this->render("pages/view.html.twig");
+    }
+    /**
+     * @Route("/post/delete/{id}", name="delete_post_route")
+     */
+    public function deletePostAction($id, Request $request) {
+        return $this->render("pages/delete.html.twig");
+    }
+
+
+}
