43df58aa685bde8887e2a2c65bbbdf0a98f495e8
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2018-02-16 11:06:23 +0200

    creating create post form

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class PostController extends Controller
 {
     /**
@@ -23,8 +27,34 @@ class PostController extends Controller
     /**
      * @Route("/post/create", name="create_post_route")
      */
-    public function createPostAction() {
-        return $this->render("pages/create.html.twig");
+    public function createPostAction(Request $request) {
+        $post = new Post;
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control')))
+            ->add('category', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Create Post','attr' => array('class' 
+                => 'btn btn-primary', 'style' => 'margin-top: 10px;')))
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $title = $form['title']->getData();
+            $description = $form['description']->getData();
+            $category = $form['category']->getData();
+
+            $post->setTitle($title);
+            $post->setDescription($description);
+            $post->setCategory($category);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            $this->addFlash('message', 'Post saved successfully');
+            return $this->redirectToRoute('view_post_route');
+        }
+        return $this->render("pages/create.html.twig", [
+            'form' => $form->createView()
+        ]);
     }
     /**
      * @Route("/post/update/{id}", name="update_post_route")
